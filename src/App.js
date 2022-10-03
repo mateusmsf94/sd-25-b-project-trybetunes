b7c18bb634b4210cfa2071afddaf71b8fccde90b
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-10-03 16:10:06 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites"><Favorites /></Route>
+        <Route exact path="/profile"><Profile /></Route>
+        <Route path="/profile/edit"><ProfileEdit /></Route>
+        <Route path="*"><NotFound /></Route>
+
+      </Switch>
+    );
   }
 }
 
