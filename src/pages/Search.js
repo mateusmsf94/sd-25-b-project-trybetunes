ec4c8a91af337faea7d1502d93a7a8063662b8ac
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-10-04 12:07:45 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,50 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      isDisable: true,
+    };
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      artistName: event.target.value,
+    }, this.buttonReady);
+  };
+
+  buttonReady = () => {
+    const MINCHAR = 2;
+    const { artistName } = this.state;
+    if (artistName.length >= MINCHAR) {
+      this.setState({
+        isDisable: false,
+      });
+    } else {
+      this.setState({
+        isDisable: true,
+      });
+    }
+  };
+
   render() {
+    const { isDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Nome do Artista"
+            onChange={ this.changeHandler }
+          />
+          <button type="submit" data-testid="search-artist-button" disabled={ isDisable }>
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
