6e6d3ba5321e1fb7801247375cdaa99ad8d9b0dc
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-10-10 22:44:33 -0300

    volta no tempo + requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const data = await getUser();
+    this.setState({
+      user: data,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { isLoading, user } = this.state;
     return (
-      <div data-testid="page-profile">
+      <>
         <Header />
-        <h1>Profile</h1>
-      </div>
+        <div data-testid="page-profile">
+          {isLoading ? <Loading /> : (
+            <div>
+              <img data-testid="profile-image" src={ user.image } alt={ user.name } />
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+              <p>{user.description}</p>
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
